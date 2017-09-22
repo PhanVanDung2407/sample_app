e0553e1fccd65a8fa4e437df836ade966ac855c9
Phan Van Dung <phanvandung2407@gmail.com>
2017-09-22 13:42:02 +0700

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by id: params[:id]
-    unless @user
-      flash[:danger] = I18n.t(".users.new.error_message")
-      render "static_pages/home"
-    end
+    return if @user
+    flash[:danger] = I18n.t(".users.new.error_message")
+    render "static_pages/home"
   end
 
   def new
@@ -14,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = I18n.t("static_pages.home.welcome")
       redirect_to @user
     else
